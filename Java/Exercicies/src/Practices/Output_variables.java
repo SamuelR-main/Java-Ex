6127f49f4bb00cf907031491ae9ff64e3bbe2b29
Samuel <saaamuelrooodrigooo@gmail.com>
2025-07-22 16:13:58 -0300

    Qs4-10

--- Java/Exercicies/src/Practices/Output_variables.java
+++ Java/Exercicies/src/Practices/Output_variables.java
@@ -14,5 +14,33 @@ public class Output_variables {
 		//#4 Create two variables int with values, sum and print
 		int num1 = 10, num2 = 15;
 		System.out.println(num1 + num2);
+		//#5 Create variable double with a number decimal and print
+		double q5Num = 10.00f;
+		System.out.println(q5Num);
+		//#6 Declare boolean variable and print value
+		boolean isTrueQ6 = true;
+		System.out.println("Question 6? " + isTrueQ6);
+		//#7 Charge value two variables and print before e later
+		int q7Num1 = 10, q7Num2 = 15;
+		System.out.println("Number: " + q7Num1 + "Number 2: " + q7Num2);
+		q7Num1 = q7Num2 = 20;
+		System.out.println("Number 1: " + q7Num1 + "Number 2: " + q7Num2);
+		//#8 Print "The student Jon he has 20 years", using variables
+		String q8Name = "Jon";
+		int q8Age = 20;
+		String q8Setence1 = "The student ";
+		String q8Setence2 = " he has ";
+		String q8Setence3 = " years";
+		System.out.println(q8Setence1 + q8Name + q8Setence2 + q8Age + q8Setence3);
+		//#9 print the result multiply two numbers
+		int q9Num1, q9Num2;
+		q9Num1 = 10;
+		q9Num2 = 15;
+		int q9Multi = q9Num1 * q9Num2;
+		System.out.println("A soma da multiplicação de " + q9Num1 + " com " + q9Num2 + " é " + q9Multi);
+		//#10 Create three variables int e print media aritmet
+		
+		//#11 Print your Name, Age and City in three lines differents
+		//#12 Print 
 	}
 }
