3d03c2d16356917842835894cf030e034468469d
Samuel <saaamuelrooodrigooo@gmail.com>
2025-07-28 16:53:34 -0300

    More Variables

--- Java/Exercicies/src/javaEx/Variable.java
+++ Java/Exercicies/src/javaEx/Variable.java
@@ -2,16 +2,32 @@ package javaEx;
 
 public class Variable {
 	public static void main(String[] args) {
-		//Numbers positives and negatives 
-		int myNum = 10;
-		//Numbers Broken, Decimals
+		//byte store 256 numbers: -128 to 127
+		byte minNum1 = -128;
+		byte maxNum1 = 127;
+		//short store 65,536 numbers: -32,768 to 32,767
+		short minNum2 = -32768;
+		short maxNum2 = 32767;
+		//Numbers positives and negatives, int store 4.294.967.296 numbers: -2147483648 to 2147483647
+		int minNum = -2147483648;
+		int maxNum = 2147483647;
+		//long store 18.446.744.073.709.551.616 numbers: -9223372036854775808 to 9223372036854775807
+		long minNum3 = -9223372036854775808L;
+		long maxNum3 = 9223372036854775807L;
+		long CPF = 9112483583L;
+		//Numbers Broken, store 6 to 7 decimal digits
+		float minNum4 = 1.101122334455667788f;
 		float decimal = 10.00f;
+		//double store 15 to 16 decimal digits
+		double minNum5 = 10.00;
 		//true or false
 		boolean isTrue = true;
+		boolean isFalse = false;
 		//letter only
 		char letter = 'a';
 		//constant 
 		final int age = 21;
+
 		//words
 		String name = "Bony ";
 		String lastName = "Meal";
